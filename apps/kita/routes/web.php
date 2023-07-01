3c5c0944648c00b47db96f304188453f89d26370
arimura <arimura@craid-inc.com>
2023-07-01 12:53:33 +0900

    ログイン、ログアウト機能の完成

--- apps/kita/routes/web.php
+++ apps/kita/routes/web.php
@@ -1,4 +1,3 @@
-web.php
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
@@ -18,11 +17,11 @@ Route::get('/', function () {
 });
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // 会員登録ルート
-Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [RegisterController::class, 'register']);
+Route::get('/member_registration', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/member_registration', [RegisterController::class, 'register']);
 // ログインルート
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
 // ログアウトルート
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
--- vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
+++ vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace Illuminate\Foundation\Auth;
-
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
-
 trait AuthenticatesUsers
 {
     use RedirectsUsers, ThrottlesLogins;
-
     /**
      * Show the application's login form.
      *
@@ -20,7 +16,6 @@ trait AuthenticatesUsers
     {
         return view('auth.login');
     }
-
     /**
      * Handle a login request to the application.
      *
@@ -32,33 +27,26 @@ trait AuthenticatesUsers
     public function login(Request $request)
     {
         $this->validateLogin($request);
-
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
         // the IP address of the client making these requests into this application.
         if (method_exists($this, 'hasTooManyLoginAttempts') &&
             $this->hasTooManyLoginAttempts($request)) {
             $this->fireLockoutEvent($request);
-
             return $this->sendLockoutResponse($request);
         }
-
         if ($this->attemptLogin($request)) {
             if ($request->hasSession()) {
                 $request->session()->put('auth.password_confirmed_at', time());
             }
-
             return $this->sendLoginResponse($request);
         }
-
         // If the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
         $this->incrementLoginAttempts($request);
-
         return $this->sendFailedLoginResponse($request);
     }
-
     /**
      * Validate the user login request.
      *
@@ -74,7 +62,6 @@ trait AuthenticatesUsers
             'password' => 'required|string',
         ]);
     }
-
     /**
      * Attempt to log the user into the application.
      *
@@ -87,7 +74,6 @@ trait AuthenticatesUsers
             $this->credentials($request), $request->boolean('remember')
         );
     }
-
     /**
      * Get the needed authorization credentials from the request.
      *
@@ -98,7 +84,6 @@ trait AuthenticatesUsers
     {
         return $request->only($this->username(), 'password');
     }
-
     /**
      * Send the response after the user was authenticated.
      *
@@ -108,18 +93,14 @@ trait AuthenticatesUsers
     protected function sendLoginResponse(Request $request)
     {
         $request->session()->regenerate();
-
         $this->clearLoginAttempts($request);
-
         if ($response = $this->authenticated($request, $this->guard()->user())) {
             return $response;
         }
-
         return $request->wantsJson()
-                    ? new JsonResponse([], 204)
-                    : redirect()->intended($this->redirectPath());
+            ? new JsonResponse([], 204)
+            : redirect()->intended($this->redirectPath());
     }
-
     /**
      * The user has been authenticated.
      *
@@ -131,7 +112,6 @@ trait AuthenticatesUsers
     {
         //
     }
-
     /**
      * Get the failed login response instance.
      *
@@ -146,7 +126,6 @@ trait AuthenticatesUsers
             $this->username() => [trans('auth.failed')],
         ]);
     }
-
     /**
      * Get the login username to be used by the controller.
      *
@@ -156,7 +135,6 @@ trait AuthenticatesUsers
     {
         return 'email';
     }
-
     /**
      * Log the user out of the application.
      *
@@ -166,20 +144,15 @@ trait AuthenticatesUsers
     public function logout(Request $request)
     {
         $this->guard()->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
         if ($response = $this->loggedOut($request)) {
             return $response;
         }
-
         return $request->wantsJson()
             ? new JsonResponse([], 204)
-            : redirect('/');
+            : redirect('auth/login');
     }
-
     /**
      * The user has logged out of the application.
      *
@@ -190,7 +163,6 @@ trait AuthenticatesUsers
     {
         //
     }
-
     /**
      * Get the guard to be used during authentication.
      *
@@ -201,3 +173,5 @@ trait AuthenticatesUsers
         return Auth::guard();
     }
 }
+
+?>
