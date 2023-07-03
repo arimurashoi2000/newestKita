fabdfcfe727254b0e82491075c2ca4e6102e7041
arimura <arimura@craid-inc.com>
2023-07-03 11:53:52 +0900

    別ブランチに移るためのコミット

--- apps/kita/database/migrations/2023_07_01_094158_create_articles_table.php
+++ apps/kita/database/migrations/2023_07_01_094158_create_articles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('contents');
             $table->unsignedBigInteger('member_id');
+            $table->foreignId('member_id');
             $table->timestamps();
             $table->softDeletes()->nullable();
         });
