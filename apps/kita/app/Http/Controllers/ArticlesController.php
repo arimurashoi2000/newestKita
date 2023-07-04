7cca66acfc880f8f99eb36c8ca5e0e497d5b9352
arimura <arimura@craid-inc.com>
2023-07-05 06:14:52 +0900

    記事検索機能

--- apps/kita/app/Http/Controllers/ArticlesController.php
+++ apps/kita/app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@ namespace app\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Member;
+use Illuminate\Http\Request;
 class ArticlesController extends Controller
 {
     //
@@ -12,5 +13,15 @@ class ArticlesController extends Controller
         $articles = Article::with('member')->paginate(10);
         return view('articles.articles', compact('articles'));
     }
+
+    public function search(Request $request) {
+        $keyword = $request->input('keyword');
+        $articles = Article::where('title', 'like', "%$keyword%")->orWhere('contents', 'like', "%$keyword%")->paginate(10);
+
+        $articles->appends(['keyword' => $keyword]); // クエリ文字列を追加
+        return view('articles.articles', compact('articles'));
+    }
+
+
 
 }
