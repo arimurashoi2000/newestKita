2ae8362bd97a4948cb9293ec7e06ee71759aec55
arimura <arimura@craid-inc.com>
2023-07-06 10:40:24 +0900

    コントローラーの修正とphpdocの追加

--- apps/kita/app/Http/Controllers/ArticlesController.php
+++ apps/kita/app/Http/Controllers/ArticlesController.php
@@ -3,12 +3,14 @@ namespace app\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Member;
+
 class ArticlesController extends Controller
 {
-    //
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index() {
-        $member_id = auth()->id();
         $articles = Article::with('member')->orderBy('updated_at', 'desc')->paginate(10);
         return view('articles.articles', compact('articles'));
     }
