326daa725cf2b8f0cbb03361d1575673f7285705
arimura <arimura@craid-inc.com>
2023-07-11 12:46:32 +0900

    記事作成機能のデザイン反映

--- apps/kita/resources/views/articles/articles.blade.php
+++ apps/kita/resources/views/articles/articles.blade.php
@@ -6,32 +6,38 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
-<body>
-{{ Form::open(['route' => 'articles.search', 'method' => 'get']) }}
-{{Form::label('search', '検索')}}
-{{Form::text('keyword', null, ['class'=>'search'])}}
-{{Form::submit('検索する', ['class'=>'search_button'])}}
-{{Form::close()}}
-
-<!--記事投稿-->
-{{ Form::open(['url' => route('articles.create'), 'method' => 'get']) }}
-{{Form::submit('記事を作成する', ['class'=>'search_button'])}}
-{{Form::close()}}
-
-@foreach ($articles as $article)
-   <p>{{$article->member->name}}</p>
-    <p>{{$article->created_at}}</p>
-    <p>{{$article->title}}</p>
-    <p>{{$article->contents}}</p>
-
-@endforeach
-<div class="mb-4">
-    <p>{{ $articles->links('pagination::bootstrap-4') }}</p>
-</div>
-
-</body>
+@include('articles.header')
+<div class="container py-5">
+    <div class="d-flex align-items-center justify-content-center bg-secondary" style="--bs-bg-opacity: .1 height: 80vh;">
+        <div class="col-md-7 col-10 bg-white text-dark">
+            <div class="card">
+                <div class="card-body px-5 py-3">
+                    @foreach ($articles as $article)
+                        <div class="row pt-3">
+                            <div class="col-md-6">
+                                <p>{{$article->member->name}}が{{ $article->created_at->format('Y年m月d日') }}に投稿<p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 col-12">
+                                <h3 class="font-weight-bold">{{$article->title}}</h3>
+                            </div>
+                        </div>
+                        <!--タグ一覧-->
+                        <div class="row">
+                            <div class="col-md-12 col-12 border-bottom border-dark py-2"></div>
+                        </div>
+                    @endforeach
+                    <div class="d-flex align-items-center justify-content-center mb-1 mt-3">
+                        <p>{{ $articles->links('pagination::bootstrap-4') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
 </html>
+</body>
--- apps/kita/resources/views/articles/articles_create.blade.php
+++ apps/kita/resources/views/articles/articles_create.blade.php
@@ -6,22 +6,63 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
+
 <body>
-{{ Form::open(['route' => 'store']) }}
-@csrf
-{{Form::label('title', 'タイトル')}}
-{{Form::text('title', null, ['class'=>'title'])}}
-{{Form::label('contents', '記事内容')}}
-{{Form::text('contents', null, ['class'=>'contents'])}}
+@include('articles.header')
+<div class="container py-5">
+    <div class="d-flex align-items-center justify-content-center bg-secondary" style="--bs-bg-opacity: .1 height: 100vh;">
+        <div class="col-md-8 col-10 bg-white text-dark">
+            <div class="card">
+                <div class="container">
+
+                </div>
+                <div class="card-body px-5 py-3">
+                    {{ Form::open(['route' => 'store']) }}
+                    @csrf
+                        <!--タイトル-->
+                        <div class="row pt-3">
+                                {{Form::label('title', 'タイトル', ['class' => 'col-md-12 col-12 px-0'])}}
+                        </div>
+
+                        <div class="row pt-3">
+                            {{Form::text('title', null, ['class'=>'title col-md-12 col-12 border border-success mt-2 rounded'])}}
+                        </div>
 
 
-{{Form::submit('投稿する', ['class'=>'submit_button'])}}
 
-{{Form::close()}}
+                        <!--タグ-->
+                        <div class="row">
+                            <div class="col-md-12 col-12"></div>
+                        </div>
 
-</body>
+                        <!--記事内容-->
+                        <div class="row pt-3">
+                                {{Form::label('contents', '記事内容', ['class'=>'col-md-12 col-12 px-0 '])}}
+                        </div>
+
+                        <div class="row">
+                                {{Form::textarea('contents', null, ['class'=>'contents col-md-12 col-12 border border-success mt-2 rounded', 'style' => 'height: 400px;'])}}
+                        </div>
+
+
+                        <!--投稿ボタン-->
+                        <div class="row">
+                            <div class="col-md-12 col-12 text-md-end px-0">
+                                {{Form::submit('投稿する', ['class'=>'submit_button btn btn-success my-3 rounded'])}}
+                            </div>
+                        </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+
+
+
 </html>
