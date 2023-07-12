31507891a7357ed934865adfa1d77048d8d5e5f0
arimura <arimura@craid-inc.com>
2023-07-12 12:39:58 +0900

    addし忘れたuntracked fileの追加

---
+++ apps/kita/database/factories/Article_commentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Article_comment;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article_comment>
+ */
+class Article_commentFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Article_comment::class;
+
+    /**
+     * @return array|mixed[]
+     */
+    public function definition()
+    {
+        return [
+            'contents' => $this->faker->text(50),
+            'member_id' => $this->faker->numberBetween(1, 10),
+            'article_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
