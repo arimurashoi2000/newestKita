a204e6c621ead980cafb73ac35b5064c1c98da8e
arimura <arimura@craid-inc.com>
2023-07-23 20:58:27 +0900

    seederファイルの追加

---
+++ apps/kita/database/seeders/MemberSeeder.php
@@ -0,0 +1,16 @@
+<?php
+namespace Database\Seeders;
+use App\Models\Member;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+class MemberSeeder extends Seeder
+{
+    /**
+     * memberシーダーを実行
+     * @return void
+     */
+    public function run()
+    {
+        Member::factory()->count(20)->create();
+    }
+}
