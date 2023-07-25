e95b7ead6f9b9c87da0b11e53e411dbfffb358e6
arimura <arimura@craid-inc.com>
2023-07-25 13:11:35 +0900

    デザイン修正

--- apps/kita/resources/views/articles/articles_create.blade.php
+++ apps/kita/resources/views/articles/articles_create.blade.php
@@ -1,57 +1,52 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>新規投稿画面</title>
 </head>
 
 <body>
-@include('articles.header')
-<div class="container py-5">
-    <div class="d-flex align-items-center justify-content-center bg-secondary" style="--bs-bg-opacity: .1 height: 100vh;">
-        <div class="col-md-8 col-10 bg-white text-dark">
-            <div class="card">
-                <div class="card-body px-5 py-3">
-                    {{ Form::open(['route' => 'store']) }}
-                    @csrf
-                        <!--タイトル-->
-                        <div class="row pt-3">
-                            {{Form::label('title', 'タイトル', ['class' => 'col-md-12 col-12 px-0'])}}
-                        </div>
-
-                        <div class="row pt-3">
-                            {{Form::text('title', null, ['class'=>'title col-md-12 col-12 border border-success mt-2 rounded'])}}
-                        </div>
-
-                        <!--タグ-->
-                        <div class="row">
-                            <div class="col-md-12 col-12"></div>
-                        </div>
-
-                        <!--記事内容-->
-                        <div class="row pt-3">
-                            {{Form::label('contents', '記事内容', ['class'=>'col-md-12 col-12 px-0 '])}}
-                        </div>
-
-                        <div class="row">
-                            {{Form::textarea('contents', null, ['class'=>'contents col-md-12 col-12 border border-success mt-2 rounded', 'style' => 'height: 400px;'])}}
-                        </div>
-
-                        <!--投稿ボタン-->
-                        <div class="row">
-                            <div class="col-md-12 col-12 text-md-end px-0">
-                                {{Form::submit('投稿する', ['class'=>'submit_button btn btn-success my-3 rounded'])}}
+@extends('articles.header')
+@section('content')
+    <div class="container py-5">
+        <div class="d-flex align-items-center justify-content-center" style="--bs-bg-opacity: .1">
+            <div class="col-md-8 col-10 bg-white text-dark">
+                <div class="card">
+                    <div class="card-body px-5 py-3">
+                        {{ Form::open(['route' => 'store']) }}
+                        @csrf
+                            <!--タイトル-->
+                            <div class="row pt-3">
+                                {{Form::label('title', 'タイトル', ['class' => 'col-md-12 col-12 px-0'])}}
                             </div>
-                        </div>
-                    {{ Form::close() }}
+
+                            <div class="row pt-3">
+                                {{Form::text('title', null, ['class'=>'title col-md-12 col-12 border border-success mt-2 rounded'])}}
+                            </div>
+
+                            <!--タグ-->
+                            <div class="row">
+                                <div class="col-md-12 col-12"></div>
+                            </div>
+
+                            <!--記事内容-->
+                            <div class="row pt-3">
+                                {{Form::label('contents', '記事内容', ['class'=>'col-md-12 col-12 px-0 '])}}
+                            </div>
+
+                            <div class="row">
+                                {{Form::textarea('contents', null, ['class'=>'contents col-md-12 col-12 border border-success mt-2 rounded', 'style' => 'height: 400px;'])}}
+                            </div>
+
+                            <!--投稿ボタン-->
+                            <div class="row">
+                                <div class="col-md-12 col-12 text-md-end px-0">
+                                    {{Form::submit('投稿する', ['class'=>'submit_button btn btn-success my-3 rounded'])}}
+                                </div>
+                            </div>
+                        {{ Form::close() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
--- apps/kita/resources/views/articles/header.blade.php
+++ apps/kita/resources/views/articles/header.blade.php
@@ -48,14 +48,8 @@
         </div>
     </div>
 </div>
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<div class="main">
+    @yield('content')
+</div>
 </body>
 </html>
