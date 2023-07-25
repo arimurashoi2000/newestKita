724c1913e7418e4cc260037fd4afcda9e9c4906b
arimura <arimura@craid-inc.com>
2023-07-25 14:57:55 +0900

    ヘッダーを適用、デザイン修正

--- apps/kita/resources/views/articles/articles_show.blade.php
+++ apps/kita/resources/views/articles/articles_show.blade.php
@@ -1,31 +1,26 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>記事詳細ページ</title>
 </head>
 
-<div class="container py-5">
-    <div class="d-flex align-items-center justify-content-center bg-secondary" style="--bs-bg-opacity: .1 height: 100vh;">
-        <div class="col-md-7 col-10 bg-white text-dark">
-            <div class="card" style="height: 800px;">
-                <div class="card-body px-5 py-3">
-                    <!--削除、編集用のボタン-->
-                    <div class="row d-flex justify-content-end">
-                        <div class="col-md-2 text-end">
-                            <a href="#" class="btn btn-danger rounded-pill">削除する</a>
-                        </div>
+@extends('articles.header')
+@section('content')
+    <div class="container py-5">
+        <div class="d-flex align-items-center justify-content-center" style="--bs-bg-opacity: .1">
+            <div class="col-md-7 col-10 bg-white text-dark">
+                <div class="card">
+                    <div class="card-body px-5 py-3">
+                        <!--削除、編集用のボタン-->
+                        <div class="row d-flex justify-content-end">
+                            <div class="col-md-2 text-end">
+                                <a href="#" class="btn btn-danger rounded-pill">削除する</a>
+                            </div>
 
-                        <div class="col-md-2 text-end">
-                            <a href="{{ route('articles.edit', ['id' => $article->id]) }}" class="btn btn-success rounded-pill">編集する</a>
+                            <div class="col-md-2 text-end">
+                                <a href="{{ route('articles.edit', ['id' => $article->id]) }}" class="btn btn-success rounded-pill">編集する</a>
+                            </div>
                         </div>
-                    </div>
-
 
                         <!--タイトル-->
                         <div class="row">
@@ -54,10 +49,9 @@
                         </div>
 
                         <!--コメント機能実装後にコメント表示-->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    </body>
-</html>
-</body>
+@endsection
