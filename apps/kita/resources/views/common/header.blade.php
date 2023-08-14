8a815b53cf82cfa1602919f69a5f80fe1f3f865d
arimura <arimura@craid-inc.com>
2023-08-14 15:58:12 +0900

    ヘッダーのログアウトボタンの修正

--- apps/kita/resources/views/common/header.blade.php
+++ apps/kita/resources/views/common/header.blade.php
@@ -34,14 +34,17 @@
                             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
                         </svg>
                     </button>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                        <li>
-                            {{ Form::open(['route' => 'logout', 'method' => 'post']) }}
-                                @csrf
-                            {{Form::submit('ログアウト', ['class'=>'dropdown-item'])}}
-                            {{ Form::close() }}
-                        </li>
-                    </ul>
+                    @if(auth()->guard('members')->check())
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                            <li>
+                                {{ Form::open(['route' => 'logout', 'method' => 'post']) }}
+                                    @csrf
+                                {{Form::submit('ログアウト', ['class'=>'dropdown-item'])}}
+                                {{ Form::close() }}
+                            </li>
+                        </ul>
+                    @else
+                    @endif
                 </div>
             </div>
         </nav>
