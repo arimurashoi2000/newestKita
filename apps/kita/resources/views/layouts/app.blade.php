34e38fbf7d8171811d48b1cccfbabf988b41748f
arimura <arimura@craid-inc.com>
2023-08-22 15:50:10 +0900

    プルリク修正

--- apps/kita/resources/views/layouts/app.blade.php
+++ apps/kita/resources/views/layouts/app.blade.php
@@ -9,16 +9,12 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
-@if (\Auth::guard('members')->check() && !request()->is(['login', 'member_registration', 'admin/*']))
+<!--ユーザー側のログイン、会員登録、管理者画面以外は会員用のヘッダーを読み込む-->
+@if (!request()->is(['login', 'member_registration', 'admin/*']))
     @include('common.header')
-@elseif (\Auth::guard('admin_users')->check() && !request()->is(['admin/login']) && request()->is(['admin/*']))
+<!--管理者側のログイン画面以外の管理者画面では管理者用のヘッダーを読み込む-->
+@elseif (!request()->is(['admin/login']) && request()->is(['admin/*']))
     @include('common.admin_header')
-@else
-    @if(!request()->is(['login', 'member_registration', 'admin/*']))
-        @include('common.header')
-    @elseif(!request()->is(['admin/*']) && !request()->is(['login', 'member_registration', 'admin/*']))
-        @include('common.admin_header')
-    @endif
 @endif
 
 @yield('content')
